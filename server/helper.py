65107fc9957db838dc07a3973e4587168517b7f2
Azhar Rais <dev@azharaiz.com>
2019-12-19 03:01:51 +0700

    server configured

---
+++ server/helper.py
@@ -0,0 +1,17 @@
+import os
+
+def get_img():
+    return os.path.join(os.path.abspath(os.getcwd()),"data","img")
+
+def get_log():
+    return os.path.join(os.path.abspath(os.getcwd()),"data","log.txt")
+
+def get_file():
+    log_file = open(get_log(), 'r')
+    arr = []
+    line = log_file.readline()
+    while line:
+        arr.append(line.split(','))
+        line = log_file.readline()
+    log_file.close()
+    return arr
--- server/server.py
+++ server/server.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template
+from helper import get_file
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
     
+@app.route("/")
+def log():
+    return render_template("log.html", logs=get_file())
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
